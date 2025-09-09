2f0743baf7986a366d9775fea2774134789a4bd4
giasinguyen <giasinguyentran@gmail.com>
2025-09-09 22:01:21 +0700

    Add global exception handling and response structure

---
+++ aurora-backend/src/main/java/com/aurora/backend/enums/ErrorCode.java
@@ -0,0 +1,27 @@
+package com.aurora.backend.enums;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_KEY(1001, "Uncategorized error", HttpStatus.BAD_REQUEST),
+    USER_EXISTED(1002, "User existed", HttpStatus.BAD_REQUEST),
+    USER_NOT_EXISTED(1003, "User not existed", HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(1004, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1005, "You do not have permission", HttpStatus.FORBIDDEN),
+    ;
+
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
+        this.code = code;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    private final int code;
+    private final String message;
+    private final HttpStatusCode statusCode;
+}
